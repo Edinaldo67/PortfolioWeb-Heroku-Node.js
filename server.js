f315a437c10635bf4b924f2aab2b6fc963701f14
Edinaldo Araujo <naldinhoo67@gmail.com>
2019-03-30 01:21:19 -0300

    modified

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+// var express = require("express");
+// var app = express();
+// app.use(express.json());
+// app
+//   .use(express.static(__dirname + "./dist/index"))
+//   .listen(process.env.PORT || 5500);
+
 var express = require("express");
-var app = express();
-app.use(express.json());
-app
-  .use(express.static(__dirname + "./dist/index"))
-  .listen(process.env.PORT || 5500);
+var path = require("path");
+var PORT = process.env.PORT || 5500;
+
+express()
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("dist/index"))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
