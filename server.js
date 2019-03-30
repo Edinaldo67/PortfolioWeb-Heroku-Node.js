b9f037540ec40ab9fb553eb4eb9a1a3563b88486
Edinaldo Araujo <naldinhoo67@gmail.com>
2019-03-30 02:28:07 -0300

    test

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 5500;
 app.get("/", function(req, res) {
-  res.send("Welcome to NodeJS app on Heroku");
+  //   res.send("Welcome to NodeJS app on Heroku");
+  res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 
 app.listen(port);
