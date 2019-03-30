32cc9b6a508a97175ec6a47d77e17dc05ff65a27
Edinaldo Araujo <naldinhoo67@gmail.com>
2019-03-30 01:11:04 -0300

    modified

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require("express");
-var app = express()
-  .use(express.static(__dirname + "dist/index.html"))
+var app = express();
+app.use(express.json());
+app
+  .use(express.static(__dirname + "./dist/index"))
   .listen(process.env.PORT || 5500);
